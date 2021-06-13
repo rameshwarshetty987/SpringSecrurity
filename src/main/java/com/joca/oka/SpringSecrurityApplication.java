ab4adfb0af4400155842fac65a3f5218565e83c4
rameshwar setty <rameshwarshetty987@gmail.com>
2021-06-13 07:32:47 +0530

    Spring security Project Setup

---
+++ src/main/java/com/joca/oka/SpringSecrurityApplication.java
@@ -0,0 +1,13 @@
+package com.joca.oka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecrurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecrurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joca/oka/SpringSecrurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joca.oka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecrurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
